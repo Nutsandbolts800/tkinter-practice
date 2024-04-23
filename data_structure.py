0280482cb69543c69dcd18d4e393e1bb37a9b4ae
Nutsandbolts800 <85801702+Nutsandbolts800@users.noreply.github.com>
2024-04-22 19:30:46 -0700

    Add files via upload

---
+++ data_structure.py
@@ -0,0 +1,23 @@
+data_structure = {}
+
+def add_data_instance(new_data):
+    if (new_data['name'] in data_structure):
+        data_structure[new_data['name']].append(new_data)
+    else:
+        data_structure[new_data['name']] = []
+        data_structure[new_data['name']].append(new_data)
+
+add_data_instance({'name': 'data1', 'time':'today','data':'6usfon457jfdsgj'})
+add_data_instance({'name': 'data2', 'time':'yesterday','data':'6uojgrs'})
+add_data_instance({'name': 'data4', 'time':'wednesday','data':'sdfgysgj'})
+add_data_instance({'name': 'data7', 'time':'tfridy','data':'ytegrsf'})
+add_data_instance({'name': 'data3', 'time':'tyh','data':'dfcvgjbh'})
+add_data_instance({'name': 'data3', 'time':'today','data':'6usfon457jfdsgj'})
+add_data_instance({'name': 'data2', 'time':'yesterday','data':'6uojgrs'})
+add_data_instance({'name': 'data4', 'time':'wednesday','data':'sdfgysgj'})
+add_data_instance({'name': 'data5', 'time':'tfridy','data':'ytegrsf'})
+add_data_instance({'name': 'data6', 'time':'tyh','data':'dfcvgjbh'})
+
+print("Data structure current state: {0}".format(data_structure))
+
+print(data_structure['data1'][0]['data'])
